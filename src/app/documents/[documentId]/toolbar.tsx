cf8a3facfbfa09a9aa45d13abf7aad0161990993
Muhammad-Amin <muhammad-amin4@mail.ru>
2024-12-11 21:30:49 +0300

    добавил размерность тексту и его изменение

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -99,8 +99,58 @@ const HeadingLevelButton = () => {
 	const headings = [
 		{ label: 'Normal text', value: 0, fontSize: '16px' },
 		{ label: 'Heading 1', value: 1, fontSize: '32px' },
-		{ label: 'Normal text', value: 0, fontSize: '16px' },
+		{ label: 'Heading 2', value: 2, fontSize: '24px' },
+		{ label: 'Heading 3', value: 3, fontSize: '20px' },
+		{ label: 'Heading 4', value: 4, fontSize: '18px' },
+		{ label: 'Heading 5', value: 5, fontSize: '16px' },
+		{ label: 'Heading 6', value: 6, fontSize: '14px' },
 	];
+
+	const getCurrentHeading = () => {
+		for (let level = 1; level <= 6; level++) {
+			if (editor?.isActive('heading', { level })) {
+				return `Heading ${level}`;
+			}
+		}
+
+		return 'Normal';
+	};
+
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<button className="text-sm h-7 min-w-7 shrink-0 flex items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden">
+					<span className="truncate">{getCurrentHeading()}</span>
+					<ChevronDownIcon className="ml-2 size-4 shrink-0" />
+				</button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent className="p-1 flex flex-col gap-y-1">
+				{headings.map(({ label, value, fontSize }) => (
+					<button
+						key={value}
+						onClick={() => {
+							if (value === 0) {
+								editor?.chain().focus().setParagraph().run();
+							} else {
+								editor
+									?.chain()
+									.focus()
+									.toggleHeading({ level: value as 1 | 2 | 3 | 4 | 5 | 6 })
+									.run();
+							}
+						}}
+						style={{ fontSize }}
+						className={cn(
+							'flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80',
+							(value === 0 && !editor?.isActive('heading')) ||
+								(editor?.isActive('heading', { level: value }) && 'bg-neutral-200/80')
+						)}>
+						{label}
+					</button>
+				))}
+			</DropdownMenuContent>
+		</DropdownMenu>
+	);
 };
 
 export const Toolbar: React.FC<Props> = ({ className }) => {
@@ -191,7 +241,7 @@ export const Toolbar: React.FC<Props> = ({ className }) => {
 			<Separator orientation="vertical" className="h-6 bg-neutral-300" />
 			<FontFamilyButton />
 			<Separator orientation="vertical" className="h-6 bg-neutral-300" />
-			{/* TODO: Heading */}
+			<HeadingLevelButton />
 			<Separator orientation="vertical" className="h-6 bg-neutral-300" />
 			{/* TODO: Font size */}
 			<Separator orientation="vertical" className="h-6 bg-neutral-300" />
