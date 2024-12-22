fa0f211b0f16132149f9bf939e4ca5958d450df4
Muhammad-Amin <muhammad-amin4@mail.ru>
2024-12-22 23:40:33 +0300

    подправил схему

--- convex/schema.ts
+++ convex/schema.ts
@@ -4,6 +4,14 @@ import { v } from 'convex/values';
 export default defineSchema({
 	documents: defineTable({
 		title: v.string(),
-		content: v.string(),
-	}),
+		initialContent: v.optional(v.string()),
+		ownerId: v.string(),
+		roomId: v.optional(v.string()),
+		organizationId: v.optional(v.string()),
+	})
+		.index('by_owner_id', ['ownerId'])
+		.index('by_organization_id', ['organizationId'])
+		.searchIndex('search_documents', { searchField: 'title' }),
 });
+
+// ownerId: v.id('users'),
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,11 +5,13 @@ import Navbar from './navbar';
 import TemplatesGallery from './templates-gallery';
 import { api } from '../../../convex/_generated/api';
 import { Button } from '@/components/ui/button';
+import DocumentsList from './documents-list';
 
 export default function Home() {
 	const documents = useQuery(api.documents.getDocuments);
 
 	console.log(documents);
+
 	return (
 		<div className="min-h-screen flex flex-col">
 			<div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
@@ -17,30 +19,13 @@ export default function Home() {
 			</div>
 			<div className="mt-16">
 				<TemplatesGallery />
-				<div className="flex flex-col gap-4 p-4">
-					{documents?.map((document) => (
-						<div
-							key={document._id}
-							className="p-4 flex justify-between items-center bg-gray-100 rounded-md">
-							<div className="flex flex-col gap-2">
-								<p className="text-sm font-medium">{document.title}</p>
-								<p className="text-xs text-gray-500">{document.content}</p>
-							</div>
-							<div className="flex justify-end gap-2">
-								<Button
-									className="bg-blue-500 text-white hover:bg-blue-600 hover:text-white"
-									variant="outline">
-									Edit
-								</Button>
-								<Button
-									className="bg-pink-700 text-white hover:bg-pink-800 hover:text-white"
-									variant="destructive">
-									Delete
-								</Button>
-							</div>
-						</div>
-					))}
-				</div>
+				{documents ? (
+					<DocumentsList documents={documents} />
+				) : (
+					<div className="flex justify-center items-center h-[calc(100vh-24rem)]">
+						<p className="text-gray-500">Loading...</p>
+					</div>
+				)}
 			</div>
 		</div>
 	);
