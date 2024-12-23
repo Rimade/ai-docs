b53da892e60b6bfe546a13ebbfeb377edad91804
Muhammad-Amin <muhammad-amin4@mail.ru>
2024-12-23 11:40:01 +0300

    добавил авторизацию через Clerk

--- src/app/(home)/navbar.tsx
+++ src/app/(home)/navbar.tsx
@@ -1,21 +1,18 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import SearchInput from './search-input';
-import { Button } from '@/components/ui/button';
-import { PlusIcon } from 'lucide-react';
+import { UserButton } from '@clerk/nextjs';
 
 export default function Navbar() {
 	return (
 		<nav className="flex justify-between items-center h-full w-full">
 			<Link href="/" className="flex gap-2 items-center shrink-0 pr-6">
-				<Image src="/icon.ico" alt="Lazy Docs" width={36} height={36} />
+				<Image src="/icons/logo.png" alt="Lazy Docs" width={36} height={36} />
 				<h3 className="text-xl font-normal">Документы</h3>
 			</Link>
 			<SearchInput />
 			<div className="flex gap-3 items-center shrink-0 pr-6">
-				<Button>
-					<PlusIcon className="w-4 h-4" />
-				</Button>
+				<UserButton />
 			</div>
 		</nav>
 	);
